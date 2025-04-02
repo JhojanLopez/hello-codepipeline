3f182ef9a714e29332ffc46e3a5909c744d3651c
JhojanLopez <jhojanlopez327@gmail.com>
2025-04-01 23:48:52 -0500

    feat: add files required to compile and deploy (env, config, scripts) and modification files such as appsepc and buildspec

---
+++ deploy/dev/scripts/start_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#loading environment variables
+source "/home/jhojan/projects/aws/hello-codepipeline/deploy/dev/envs/.env" #used developing
+#source "/home/ec2-user/app/envs/.env"
+# define variables
+APP_DIR=${APP_DIR}
+JAR_FILE=${JAR_FILE}
+SPRING_PROFILE=${PROFILE}
+CONFIG_FILE=${CONFIG_FILE}
+LOG_FILE=$APP_DIR/app.log
+
+# start application
+nohup java -jar "$JAR_FILE" --spring.config.location="file:$CONFIG_FILE" --spring.profiles.active="$SPRING_PROFILE" > "$LOG_FILE" 2>&1 &
+
+echo "Server started with:"
+echo "  Profile       : $SPRING_PROFILE"
+echo "  Config file   : $CONFIG_FILE"
+echo "  JAR file      : $JAR_FILE"
+echo "  Log file      : $LOG_FILE"
---
+++ deploy/dev/scripts/stop_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Cargar las variables de entorno desde .env
+source "/home/jhojan/projects/aws/hello-codepipeline/deploy/dev/envs/.env" #used developing
+#source "/home/ec2-user/app/envs/.env"
+
+# Buscar el proceso del JAR que está corriendo
+PROCESS_ID=$(pgrep -f "$JAR_FILE")
+
+# Si se encuentra el proceso, lo matamos
+if [ -z "$PROCESS_ID" ]; then
+    echo "No se encontró el proceso del JAR en ejecución."
+else
+    echo "Deteniendo el proceso del JAR (PID: $PROCESS_ID)..."
+    kill -9 "$PROCESS_ID"
+    echo "Proceso detenido."
+fi
